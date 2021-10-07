66cf775711ad60ecc8dad7d17793b8e6d694b713
bou3dila <bou3dilafiras@gmail.com>
2021-10-07 09:34:38 +0100

    first commit

---
+++ src/app/fournisseur-component/fournisseur-component.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fournisseur-component',
+  templateUrl: './fournisseur-component.component.html',
+  styleUrls: ['./fournisseur-component.component.css']
+})
+export class FournisseurComponentComponent implements OnInit {
+    idFournisseur: any;
+    code :any;
+    libelle :any;
+    hide:boolean=false;
+  constructor() { }
+
+  ngOnInit(): void {
+    this.idFournisseur = 105;
+    this.code = 'A104B89';
+    this.libelle = 'MyTeck';
+  }
+  delete(){
+    this.hide=true;
+  }
+}
